57b1131b03dd7f009d0f72e3267e2bc94ff025f0
usinia <usinia0103@gmail.com>
2023-04-12 19:52:00 +0900

    [#1] 6-6

--- ch6/6-6.js
+++ ch6/6-6.js
@@ -1,4 +1,20 @@
-let defaultOwner = { firstName: '마틴', lastName: '파울러' };
+class Person {
+  #firstName;
+  #lastName;
+  constructor(data) {
+    this.#firstName = data.firstName;
+    this.#lastName = data.lastName;
+  }
+  get firstName() {
+    return this.#firstName;
+  }
+  get lastName() {
+    return this.#lastName;
+  }
+}
+
+// let defaultOwner = { firstName: "마틴", lastName: "파울러" };
+let defaultOwner = new Person({ firstName: "마틴", lastName: "파울러" });
 
 export function getDefaultOwner() {
   return defaultOwner;
