0470c936c371c9309b045a9cd9461f5692ba5e11
usinia <usinia0103@gmail.com>
2023-04-12 20:51:43 +0900

    [#1] 6-11-2 (self)

--- ch6/6-11-2.js
+++ ch6/6-11-2.js
@@ -1,18 +1,28 @@
-import fs from 'fs';
+import fs from "fs";
 
-if (!process.argv[2]) {
-  throw new Error('파일 이름을 입력하세요');
+const fileName = process.argv[2];
+const orders = openFile(fileName);
+const isReady = process.argv.includes("-r");
+console.log(countsOrders(isReady));
+
+function openFile(name) {
+  if (!name) {
+    throw new Error("파일 이름을 입력하세요");
+  }
+
+  const fileName = `./${process.argv[2]}.json`;
+  if (!fs.existsSync(fileName)) {
+    throw new Error("파일이 존재하지 않습니다");
+  }
+
+  const rawData = fs.readFileSync(fileName);
+  return JSON.parse(rawData);
 }
 
-const fileName = `./${process.argv[2]}.json`;
-if (!fs.existsSync(fileName)) {
-  throw new Error('파일이 존재하지 않습니다');
-}
-
-const rawData = fs.readFileSync(fileName);
-const orders = JSON.parse(rawData);
-if (process.argv.includes('-r')) {
-  console.log(orders.filter((order) => order.status === 'ready').length);
-} else {
-  console.log(orders.length);
+function countsOrders(onlyCountReady = false) {
+  if (onlyCountReady) {
+    return orders.filter((order) => order.status === "ready").length;
+  } else {
+    return orders.length;
+  }
 }
