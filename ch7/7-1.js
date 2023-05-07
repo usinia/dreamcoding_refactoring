abf24e136e32ba746ba04ba48ede02e17a456c22
usinia <usinia0103@gmail.com>
2023-05-07 18:26:33 +0900

    [#2] 7-1

--- ch7/7-1.js
+++ ch7/7-1.js
@@ -1,5 +1,36 @@
-const organization = { name: 'Acme Gooseberries', country: 'GB' };
+class Organization {
+  #name;
+  #country;
+  #data;
+  constructor(data) {
+    this.#name = data.name;
+    this.#country = data.country;
+    this.#data = data;
+  }
+  get name() {
+    return this.#name;
+  }
+  set name(value) {
+    this.#name = value;
+  }
+  get country() {
+    return this.#country;
+  }
+  set country(value) {
+    this.#country = value;
+  }
+  get rawData() {
+    // return { ...this.#data };
+    return { name: this.#name, country: this.#country };
+  }
+}
 
-organization.name = 'Dream Coding';
+// const organization = { name: "Acme Gooseberries", country: "GB" };
+const organization = new Organization({
+  name: "Acme Gooseberries",
+  country: "GB",
+});
+
+organization.name = "Dream Coding";
 console.log(organization.name);
 console.log(organization.country);
