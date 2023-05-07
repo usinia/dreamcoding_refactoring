9618cc7c457c6db8fdbb78bdfd5abd6450c0fde2
usinia <usinia0103@gmail.com>
2023-05-07 19:14:59 +0900

    [#2] 7-4

--- ch7/7-4.js
+++ ch7/7-4.js
@@ -5,11 +5,13 @@ class Order {
     this.#quantity = quantity;
     this.#item = item;
   }
-
+  get basePrice() {
+    return this.#quantity * this.#item.price;
+  }
+  get discountFactor() {
+    return this.basePrice > 1000 ? 0.95 : 0.98;
+  }
   get price() {
-    const basePrice = this.#quantity * this.#item.price;
-    const discountFactor = 0.98;
-    if (basePrice > 1000) discountFactor -= 0.03;
-    return basePrice * discountFactor;
+    return this.basePrice * this.discountFactor;
   }
 }
