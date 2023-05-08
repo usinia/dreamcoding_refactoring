abe2d6bbadfba55094775f2c3e572c5de922162e
usinia <usinia0103@gmail.com>
2023-05-08 13:29:57 +0900

    [#3] 8-7

--- ch8/8-7.js
+++ ch8/8-7.js
@@ -1,10 +1,21 @@
 export function reportYoungestAgeAndTotalSalary(people) {
-  let youngest = people[0] ? people[0].age : Infinity;
-  let totalSalary = 0;
-  for (const p of people) {
-    if (p.age < youngest) youngest = p.age;
-    totalSalary += p.salary;
+  return `youngestAge: ${youngestAge()}, totalSalary: ${totalSalary()}`;
+
+  function youngestAge() {
+    const youngest = Math.min(...people.map((p) => p.age));
+    // let youngest = people[0] ? people[0].age : Infinity;
+    // for (const p of people) {
+    //   if (p.age < youngest) youngest = p.age;
+    // }
+    return youngest;
   }
 
-  return `youngestAge: ${youngest}, totalSalary: ${totalSalary}`;
+  function totalSalary() {
+    const totalSalary = people.reduce((total, p) => (total += p.salary), 0);
+    // let totalSalary = 0;
+    // for (const p of people) {
+    //   totalSalary += p.salary;
+    // }
+    return totalSalary;
+  }
 }
