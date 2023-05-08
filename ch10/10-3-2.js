44d26616dc514f432651f077f9d9ee9729736509
usinia <usinia0103@gmail.com>
2023-05-08 15:13:55 +0900

    [#5] 10-3

--- ch10/10-3-2.js
+++ ch10/10-3-2.js
@@ -1,11 +1,17 @@
 export function adjustedCapital(instrument) {
-  let result = 0;
-  if (instrument.capital > 0) {
-    if (instrument.interestRate > 0 && instrument.duration > 0) {
-      result =
-        (instrument.income / instrument.duration) *
-        anInstrument.adjustmentFactor;
-    }
+  if (!isEligibleForAdjustedCapital()) {
+    return 0;
   }
-  return result;
+
+  return (
+    (instrument.income / instrument.duration) * instrument.adjustmentFactor
+  );
+}
+
+function isEligibleForAdjustedCapital(instrument) {
+  return (
+    instrument.capital > 0 &&
+    instrument.interestRate > 0 &&
+    instrument.duration > 0
+  );
 }
