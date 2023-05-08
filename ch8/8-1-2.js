e4cdce14c5dad0a1efb9539430c10acf798754fc
usinia <usinia0103@gmail.com>
2023-05-08 13:01:08 +0900

    [#3] 8-1

--- ch8/8-1-2.js
+++ ch8/8-1-2.js
@@ -6,16 +6,9 @@ export class Account {
 
   get bankCharge() {
     let result = 4.5;
-    if (this._daysOverdrawn > 0) result += this.overdraftCharge;
+    if (this._daysOverdrawn > 0)
+      result += this.type.overdraftCharge(this._daysOverdrawn);
     return result;
-  }
-
-  get overdraftCharge() {
-    if (this.type.isPremium) {
-      const baseCharge = 10;
-      if (this._daysOverdrawn <= 7) return baseCharge;
-      else return baseCharge + (this._daysOverdrawn - 7) * 0.85;
-    } else return this._daysOverdrawn * 1.75;
   }
 
   get daysOverdrawn() {
@@ -28,6 +21,16 @@ export class AccountType {
     this._type = type;
   }
   get isPremium() {
-    return this._type === 'Premium';
+    return this._type === "Premium";
+  }
+
+  overdraftCharge(daysOverdrawn) {
+    if (!this.isPremium) {
+      return daysOverdrawn * 1.75;
+    }
+    const baseCharge = 10;
+    return daysOverdrawn <= 7
+      ? baseCharge
+      : baseCharge + (daysOverdrawn - 7) * 0.85;
   }
 }
