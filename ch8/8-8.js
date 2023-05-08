31a1348f91792e353b6f4fc88e3c0fae7757b78d
usinia <usinia0103@gmail.com>
2023-05-08 13:44:47 +0900

    [#3] 8-8

--- ch8/8-8.js
+++ ch8/8-8.js
@@ -1,5 +1,5 @@
 export function acquireData(input) {
-  const lines = input.split('\n');
+  const lines = input.split("\n");
   let firstLine = true;
   const result = [];
   for (const line of lines) {
@@ -7,13 +7,29 @@ export function acquireData(input) {
       firstLine = false;
       continue;
     }
-    if (line.trim() === '') continue;
-    const record = line.split(',');
-    if (record[1].trim() === 'India') {
+    if (line.trim() === "") continue;
+    const record = line.split(",");
+    if (record[1].trim() === "India") {
       result.push({ city: record[0].trim(), phone: record[2].trim() });
     }
   }
   return result;
+}
+
+function acquireData2(input) {
+  return input
+    .split("\n")
+    .filter((v, i) => i > 0 && !!v)
+    .map((v) => {
+      const [office, country, telephone] = v.split(",");
+      return {
+        office: office.trim(),
+        country: country.trim(),
+        telephone: telephone.trim(),
+      };
+    })
+    .filter((v) => v.country === "India")
+    .map((v) => ({ office: v.office, telephone: v.telephone }));
 }
 
 const input = `office, country, telephone\n
@@ -24,3 +40,6 @@ Porto Alegre, Brazil, +55 51 3079 3550\n
 Chennai, India, +91 44 660 44766`;
 const result = acquireData(input);
 console.log(result);
+
+const result2 = acquireData2(input);
+console.log(result2);
