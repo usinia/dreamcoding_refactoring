74324166b8dc1f9f1063d3476f21508b86f5fd08
usinia <usinia0103@gmail.com>
2023-05-08 19:33:49 +0900

    [#7] 12-7

--- ch12/12-7.js
+++ ch12/12-7.js
@@ -1,53 +1,48 @@
 class Person {
   #name;
-  constructor(name) {
+  #gender;
+  constructor(name, gender) {
     this.#name = name;
+    this.#gender = gender;
   }
 
   get name() {
     return this.#name;
   }
 
-  get genderCode() {
-    return 'X';
+  get gender() {
+    return this.#gender;
   }
-}
 
-class Male extends Person {
-  get genderCode() {
-    return 'M';
+  get isMale() {
+    return this.#gender === "M";
   }
-}
 
-class Female extends Person {
-  get genderCode() {
-    return 'F';
+  static create(record) {
+    switch (record.gender) {
+      case "M":
+        return new Person(record.name, "M");
+      case "F":
+        return new Person(record.name, "F");
+      default:
+        return new Person(record.name, "X");
+    }
   }
 }
 
 function loadFromInput(data) {
   const result = [];
   data.forEach((record) => {
-    let person;
-    switch (record.gender) {
-      case 'M':
-        person = new Male(record.name);
-        break;
-      case 'F':
-        person = new Female(record.name);
-        break;
-      default:
-        person = new Person(record.name);
-    }
+    const person = Person.create(record);
     result.push(person);
   });
   return result;
 }
 
 const people = loadFromInput([
-  { name: '엘리', gender: 'F' },
-  { name: '철수', gender: 'M' },
-  { name: '밥', gender: 'M' },
+  { name: "엘리", gender: "F" },
+  { name: "철수", gender: "M" },
+  { name: "밥", gender: "M" },
 ]);
-const numberOfMales = people.filter((p) => p instanceof Male).length;
+const numberOfMales = people.filter((p) => p.isMale).length;
 console.log(numberOfMales);
