b3ef2be551b5f667d0d62441403bb307d5eff28b
usinia <usinia0103@gmail.com>
2023-05-08 17:59:19 +0900

    [#6] 11-8

--- ch11/11-8.js
+++ ch11/11-8.js
@@ -1,5 +1,6 @@
 export class Employee {
   constructor(name, typeCode) {
+    // instance 만드는 로직을 캡슐화하고, 외부에서는 손쉽고 간결하게 instance 생성하기 위해 factory 함수 사용
     this._name = name;
     this._typeCode = typeCode;
   }
@@ -12,6 +13,22 @@ export class Employee {
   }
 
   static get legalTypeCodes() {
-    return { E: 'Engineer', M: 'Manager', S: 'Salesman' };
+    return { E: "Engineer", M: "Manager", S: "Salesman" };
+  }
+
+  //  factory 함수
+  static createEngineer(name) {
+    return new Employee(name, "E");
+  }
+
+  static createSeniorEngieer(name) {
+    return new Employee(name, "SE");
+  }
+
+  static createMarketer(name) {
+    return new Employee(name, "M");
   }
 }
+
+const employee = Employee.createEngineer("usinia");
+console.log(employee);
