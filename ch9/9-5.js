31605d4e929fd0cc0a2f87938193dd127f8a2a74
usinia <usinia0103@gmail.com>
2023-05-08 14:39:29 +0900

    [#4] 9-5

--- ch9/9-5.js
+++ ch9/9-5.js
@@ -1,7 +1,13 @@
+const customerRepository = new CustomerRepository();
+const order = new Order(
+  data.number,
+  customerRepository.registerCustomer(data.customerId)
+);
+
 class Order {
-  constructor(data) {
+  constructor(data, customer) {
     this._number = data.number;
-    this._customer = new Customer(data.customerId);
+    this._customer = customer;
   }
 
   get customer() {
@@ -10,11 +16,28 @@ class Order {
 }
 
 class Customer {
-  constructor(id) {
+  constructor(id, name) {
     this._id = id;
+    this._name = name;
   }
 
   get id() {
     return this._id;
+  }
+}
+
+class CustomerRepository {
+  #customer;
+  constructor() {
+    this.#customer = new Map();
+  }
+  registerCustomer(id) {
+    if (!this.#customer.has(id)) {
+      this.#customer.set(id, new Customer(id));
+    }
+    return findCustomer(id);
+  }
+  findCustomer(id) {
+    return this.#customer.get(id);
   }
 }
